20ed02b2e04419105e3108c5f02a1dd01721ffb8
Louis DeJardin <louis.dejardin@gmail.com>
2008-06-17 05:27:51 +0000

    Adding simple build command
    git-svn-id: http://dev.dejardin.org/svn/spark/trunk@37 0f556331-6e29-4ecb-911f-9ab3f335dbaa

---
+++ build.cmd
@@ -0,0 +1,2 @@
+%systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/net-3.5/ src/Castle.MonoRail.Views.Spark/Castle.MonoRail.Views.Spark.csproj
+pause
