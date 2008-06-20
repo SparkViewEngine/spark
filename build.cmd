1fec3551d4c021fd12d1c6fc29eadcd243c5aecf
Louis DeJardin <louis.dejardin@gmail.com>
2008-06-20 04:25:24 +0000

    Changing to project references
    git-svn-id: http://dev.dejardin.org/svn/spark/trunk@45 0f556331-6e29-4ecb-911f-9ab3f335dbaa

--- build.cmd
+++ build.cmd
@@ -1,2 +1,4 @@
-%systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/net-3.5/ src/Castle.MonoRail.Views.Spark/Castle.MonoRail.Views.Spark.csproj
+%systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/spark/ src/Spark/Spark.csproj
+%systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/castle/ src/Castle.MonoRail.Views.Spark/Castle.MonoRail.Views.Spark.csproj
+%systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/aspnetmvc/ src/MvcContrib.SparkViewEngine/MvcContrib.SparkViewEngine.csproj
 pause
