e5313a53517ca15a64e8e8112eee83799a737a0e
Louis DeJardin <louis.dejardin@gmail.com>
2008-07-07 19:29:54 +0000

    Updating build.cmd to create build/samples/NorthwindDemo also
    git-svn-id: http://dev.dejardin.org/svn/spark/trunk@53 0f556331-6e29-4ecb-911f-9ab3f335dbaa

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 %systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/spark/ src/Spark/Spark.csproj
 %systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/castle/ src/Castle.MonoRail.Views.Spark/Castle.MonoRail.Views.Spark.csproj
 %systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild /property:Configuration=Release;OutDir=../../build/aspnetmvc/ src/MvcContrib.SparkViewEngine/MvcContrib.SparkViewEngine.csproj
+%systemroot%\microsoft.net\framework\v3.5\msbuild.exe /t:Rebuild;_CopyWebApplication /property:Configuration=Release;OutDir=../../build/samples/NorthwindDemo/bin/;WebProjectOutputDir=../../build/samples/NorthwindDemo/ src/NorthwindDemo/NorthwindDemo.csproj
 pause
