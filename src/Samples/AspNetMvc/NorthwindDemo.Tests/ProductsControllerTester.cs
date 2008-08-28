e662198fa8bf53f6c9627ab8595a88f305d25811
Louis DeJardin <louis.dejardin@gmail.com>
2008-08-28 01:39:42 +0000

    MovingMoving NorthwindDemo to Samples
    git-svn-id: http://dev.dejardin.org/svn/spark/trunk@105 0f556331-6e29-4ecb-911f-9ab3f335dbaa

--- src/Samples/AspNetMvc/NorthwindDemo.Tests/ProductsControllerTester.cs
+++ src/Samples/AspNetMvc/NorthwindDemo.Tests/ProductsControllerTester.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using MvcContrib.SparkViewEngine;
+using MvcContrib.ViewFactories;
+using NorthwindDemo.Controllers;
 using NUnit.Framework;
 using Spark;
 using Spark.FileSystem;
@@ -22,11 +24,42 @@ namespace NorthwindDemo.Tests
             var controllerFactory = (SparkControllerFactory)builder.GetControllerFactory();
 
             // Use the spark view factory to initialize an engine
-            var viewFactory = new SparkViewFactory(controllerFactory.Settings);
+            var viewFactory = new SparkViewFactory(controllerFactory.Settings)
+                                  {
+                                      ViewSourceLoader = new FileSystemViewSourceLoader(@"..\..\..\NorthwindDemo\Views")
+                                  };
 
-            // Take the engine and correct the path to the views folder
-            var engine = viewFactory.Engine;
-            engine.ViewFolder = new FileSystemViewFolder(@"..\..\..\NorthwindDemo\Views");
+            var batch = new SparkBatchDescriptor();
+            batch.For<ProductsController>();
+            viewFactory.Precompile(batch);
+        }
+
+        [Test]
+        public void TestCompiledViewsWithJustEngine()
+        {
+            // Make the same settings the web app provides.
+            // (Assemblies are forced to be added because many aren't loaded 
+            // in the nunit appdomain.)
+            var settings = new SparkSettings()
+                .SetDebug(false)
+                .SetPageBaseType("MvcContrib.SparkViewEngine.SparkView")
+                .AddNamespace("System")
+                .AddNamespace("System.Collections.Generic")
+                .AddNamespace("System.Linq")
+                .AddNamespace("System.Web.Mvc")
+                .AddNamespace("NorthwindDemo.Models")
+                .AddNamespace("NorthwindDemo.Views.Helpers")
+                .AddAssembly("NorthwindDemo")
+                .AddAssembly("System.Web.Mvc")
+                .AddAssembly("MvcContrib")
+                .AddAssembly("MvcContrib.SparkViewEngine")
+                .AddAssembly("System.Web.Routing");
+
+            // create an engine
+            var engine = new SparkViewEngine(settings)
+                             {
+                                 ViewFolder = new FileSystemViewFolder(@"..\..\..\NorthwindDemo\Views")
+                             };
 
             // Generate and compile a bunch of views
             engine.CreateEntry(new SparkViewDescriptor()
@@ -48,56 +81,7 @@ namespace NorthwindDemo.Tests
                                    .SetTargetNamespace("NorthwindDemo.Controllers")
                                    .AddTemplate("Products/ListingByCategory.spark")
                                    .AddTemplate("Shared/Application.spark"));
+
         }
-
-[Test]
-public void TestCompiledViewsWithJustEngine()
-{
-    // Make the same settings the web app provides.
-    // (Assemblies are forced to be added because many aren't loaded 
-    // in the nunit appdomain.)
-    var settings = new SparkSettings()
-        .SetDebug(false)
-        .SetPageBaseType("MvcContrib.SparkViewEngine.SparkView")
-        .AddNamespace("System")
-        .AddNamespace("System.Collections.Generic")
-        .AddNamespace("System.Linq")
-        .AddNamespace("System.Web.Mvc")
-        .AddNamespace("NorthwindDemo.Models")
-        .AddNamespace("NorthwindDemo.Views.Helpers")
-        .AddAssembly("NorthwindDemo")
-        .AddAssembly("System.Web.Mvc")
-        .AddAssembly("MvcContrib")
-        .AddAssembly("MvcContrib.SparkViewEngine")
-        .AddAssembly("System.Web.Routing");
-    
-    // create an engine
-    var engine = new SparkViewEngine(settings)
-                     {
-                         ViewFolder = new FileSystemViewFolder(@"..\..\..\NorthwindDemo\Views")
-                     };
-
-    // Generate and compile a bunch of views
-    engine.CreateEntry(new SparkViewDescriptor()
-                           .SetTargetNamespace("NorthwindDemo.Controllers")
-                           .AddTemplate("Products/Edit.spark")
-                           .AddTemplate("Shared/Application.spark"));
-
-    engine.CreateEntry(new SparkViewDescriptor()
-                           .SetTargetNamespace("NorthwindDemo.Controllers")
-                           .AddTemplate("Products/Categories.spark")
-                           .AddTemplate("Shared/Application.spark"));
-
-    engine.CreateEntry(new SparkViewDescriptor()
-                           .SetTargetNamespace("NorthwindDemo.Controllers")
-                           .AddTemplate("Products/Detail.spark")
-                           .AddTemplate("Shared/Application.spark"));
-
-    engine.CreateEntry(new SparkViewDescriptor()
-                           .SetTargetNamespace("NorthwindDemo.Controllers")
-                           .AddTemplate("Products/ListingByCategory.spark")
-                           .AddTemplate("Shared/Application.spark"));
-    
-}
     }
 }
