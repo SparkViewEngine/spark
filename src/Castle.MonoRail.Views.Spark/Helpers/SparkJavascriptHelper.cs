ad8c3f16bdc0306cf04e854efa3e0230618a59a2
Daniel Tabuenca <dan@eleutian.com>
2009-03-30 10:23:09 +0800

    Added SparkJavascriptHelper to output compiled javascript spark views.
    Signed-off-by: Louis DeJardin <louis.dejardin@gmail.com>

---
+++ src/Castle.MonoRail.Views.Spark/Helpers/SparkJavascriptHelper.cs
@@ -0,0 +1,26 @@
+using Castle.MonoRail.Framework.Helpers;
+using Castle.MonoRail.Views.Spark.Wrappers;
+using Spark;
+
+namespace Castle.MonoRail.Views.Spark.Helpers
+{
+  public class SparkJavascriptHelper : AbstractHelper
+  {
+    public virtual string CompileView()
+    {
+      return CompileView(ControllerContext.SelectedViewName);
+    }
+
+    public virtual string CompileView(string view)
+    {
+      var viewFactory = new SparkViewFactory();
+      var descriptor = new SparkViewDescriptor {Language = LanguageType.Javascript};
+
+      descriptor.AddTemplate(string.Format("{0}\\{1}", ControllerContext.ViewFolder, view));
+      ((IViewSourceLoaderContainer)viewFactory).ViewSourceLoader = Context.Services.ViewSourceLoader;
+
+      var entry = viewFactory.Engine.CreateEntry(descriptor);
+      return entry.SourceCode;
+    }
+  }
+}
