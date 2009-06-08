98be032a282e28726b237c6decb0e3e95fb66ca2
Louis DeJardin <louis.dejardin@gmail.com>
2009-06-08 18:56:09 -0500

    Porting AdvancedPartials sample to AspNetMvc
    It's a pretty good sample for using named content in ways that approximate
    simple server controls or ViewComponent usage. Existed as a MonoRail
    sample previously.

---
+++ src/Samples/AspNetMvc/AdvancedPartials/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdvancedPartials.Models;
+
+namespace AdvancedPartials.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult AddingAtPlaceholders()
+        {
+            return View();
+        }
+
+        public ActionResult StyleGuide()
+        {
+            return View();
+        }
+
+        public ActionResult Boxes()
+        {
+            return View();
+        }
+
+        public ActionResult PagingAndRepeater(int? id)
+        {
+            var pageNumber = id ?? 1;
+            var pageSize = 10;
+
+            var repos = new BirdRepository();
+            var allBirds = repos.GetBirds();
+            var showBirds = allBirds.Skip((pageNumber - 1)*pageSize).Take(pageSize);
+            var birdCount = allBirds.Count();
+
+            return View(new Page<Bird>
+                        {
+                            Items = showBirds,
+                            ItemCount = birdCount,
+
+                            CurrentPage = pageNumber,
+                            PageCount = (birdCount + pageSize - 1)/pageSize,
+
+                            FirstItemIndex = (pageNumber - 1)*pageSize
+                        });
+        }
+    }
+}
---
+++ src/Samples/AspNetMvc/AdvancedPartials/Models/Page.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdvancedPartials.Models
+{
+    public class Page<TItem> : IEnumerable<TItem>
+    {
+        public IEnumerable<TItem> Items { get; set; }
+        public int ItemCount { get; set; }
+
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+
+        public int FirstItemIndex { get; set; }
+
+        public bool HasPreviousPage { get { return CurrentPage > 1; } }
+        public bool HasNextPage { get { return CurrentPage < PageCount; } }
+
+
+        public IEnumerator<TItem> GetEnumerator() { return Items.GetEnumerator(); }
+        IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+    }
+}
