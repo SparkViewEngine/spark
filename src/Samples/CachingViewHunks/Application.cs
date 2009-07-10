85322728496dadbfbc79743bd1f141252a77c0f3
Louis DeJardin <louis.dejardin@gmail.com>
2009-07-10 00:12:45 -0500

    Adding missed files to caching example
    Source control getting a little crazy somehow.

---
+++ src/Samples/CachingViewHunks/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Spark.Web.Mvc;
+
+namespace CachingViewHunks
+{
+    public class Application
+    {
+        public void RegisterViewEngine(ICollection<IViewEngine> engines)
+        {
+            SparkEngineStarter.RegisterViewEngine(engines);
+        }
+
+        public void RegisterRoutes(ICollection<RouteBase> routes)
+        {
+            routes.Add(new Route("{controller}/{action}/{id}", new MvcRouteHandler())
+            {
+                Defaults = new RouteValueDictionary(new { controller = "Home", action = "Index", id = "" }),
+            });
+        }
+    }
+}
---
+++ src/Samples/CachingViewHunks/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace CachingViewHunks
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var application = new Application();
+            application.RegisterRoutes(RouteTable.Routes);
+            application.RegisterViewEngine(ViewEngines.Engines);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
