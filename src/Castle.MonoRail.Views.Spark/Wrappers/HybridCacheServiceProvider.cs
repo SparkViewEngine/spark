57f623b66f862aa5cd4c7355bbca8ba446bd6333
Louis DeJardin <louis.dejardin@gmail.com>
2009-07-29 13:45:55 -0500

    Moving HybridCacheService into it's own file
    No reason for these to be physically together.

--- src/Castle.MonoRail.Views.Spark/Wrappers/HybridCacheServiceProvider.cs
+++ src/Castle.MonoRail.Views.Spark/Wrappers/HybridCacheServiceProvider.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Castle.MonoRail.Framework;
 using Spark;
-using Spark.Caching;
 
 namespace Castle.MonoRail.Views.Spark.Wrappers
 {
@@ -17,99 +15,6 @@ namespace Castle.MonoRail.Views.Spark.Wrappers
         public ICacheService GetCacheService(IEngineContext context)
         {
             return new HybridCacheService(context);
-        }
-    }
-
-    public class HybridCacheService : ICacheService
-    {
-        private readonly ICacheProvider _monorailCacheProvider;
-        private readonly ICacheService _fallbackCacheService;
-
-        public HybridCacheService(IEngineContext context)
-        {
-            if (context == null ||
-                context.Services == null ||
-                context.Services.CacheProvider == null)
-            {
-                _monorailCacheProvider = new NullCacheProvider();
-            }
-            else
-            {
-                _monorailCacheProvider = context.Services.CacheProvider;
-            }
-
-
-            if (context == null ||
-                context.UnderlyingContext == null ||
-                context.UnderlyingContext.Cache == null)
-            {
-                _fallbackCacheService = new NullCacheService();
-            }
-            else
-            {
-                _fallbackCacheService = new DefaultCacheService(context.UnderlyingContext.Cache);
-            }
-        }
-
-        public class NullCacheProvider : ICacheProvider
-        {
-            public void Service(IMonoRailServices serviceProvider) { }
-            public bool HasKey(string key) { return false; }
-            public object Get(string key) { return null; }
-            public void Store(string key, object data) { }
-            public void Delete(string key) { }
-        }
-
-        public object Get(string identifier)
-        {
-            // try the MR cache service, and fallback to the httpcontext cache
-            return
-                _monorailCacheProvider.Get(identifier) ??
-                _fallbackCacheService.Get(identifier);
-        }
-
-        public void Store(string identifier, CacheExpires expires, ICacheSignal signal, object item)
-        {
-            if (IsSimpleCaching(expires, signal))
-            {
-                // use MR cache service for simple identifier-only caching
-                _monorailCacheProvider.Store(identifier, item);
-            }
-            else
-            {
-                // use the httpcontext cache when storing with expires or signal args in effect
-                _fallbackCacheService.Store(identifier, expires, signal, item);
-            }
-        }
-
-
-        private static bool IsSimpleCaching(CacheExpires expires, ICacheSignal signal)
-        {
-            // signal provided - complex caching
-            if (signal != null)
-                return false;
-
-            // no signal, no expires - simple caching
-            if (expires == null)
-                return true;
-
-            // expires absolute has meaningful value - complex caching
-            if (expires.Absolute != CacheExpires.NoAbsoluteExpiration &&
-                expires.Absolute != DateTime.MaxValue)
-            {
-                return false;
-            }
-
-            // expires sliding has meaningful value - complex caching
-            if (expires.Sliding != CacheExpires.NoSlidingExpiration &&
-                expires.Sliding != TimeSpan.MaxValue &&
-                expires.Sliding != TimeSpan.Zero)
-            {
-                return false;
-            }
-
-            // no signal, no meaningful expires value - simple caching
-            return true;
         }
     }
 }
