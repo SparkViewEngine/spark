79585d1eec105bff44f7ba7671c04845faeda275
RobertTheGrey <robertgreyling@cozwecan.com>
2010-03-21 12:14:47 +0000

    Added ContentType definition

---
+++ src/SparkSense/ContentType/SparkFileAndContentTypes.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SparkSense.ContentType
+{
+    internal static class SparkFileAndContentTypes
+    {
+        [Export] [Name("spark")] [BaseDefinition("text")] 
+        internal static ContentTypeDefinition SparkContentTypeDefinition;
+
+        [Export] [ContentType("spark")] [FileExtension(".spark")] 
+        internal static FileExtensionToContentTypeDefinition SparkFileExtensionDefinition;
+    }
+}
