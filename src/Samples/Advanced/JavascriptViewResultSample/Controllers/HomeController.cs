8f705b86a2245e69457bf1baee565886e83a1611
RobertTheGrey <robertgreyling@cozwecan.com>
2010-07-22 01:08:24 +0100

    JavaScriptViewResult Done

---
+++ src/Samples/Advanced/JavascriptViewResultSample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using JavascriptViewResultSample.Models;
+using JavascriptViewResultSample.Components.Paging;
+using JavascriptViewResultSample.Services.Data;
+using Spark.Web.Mvc;
+
+namespace JavascriptViewResultSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List(int? page)
+        {
+            var pager = new Pager(page, 24, 6);
+            PagedList<RegisteredUser> RegisteredUserList = InMemoryDataService.GetAll(pager);
+            ViewData["list"] = RegisteredUserList;
+            return View(RegisteredUserList);
+        }
+
+        public ActionResult ListPage(int page)
+        {
+            var pager = new Pager(page, 24, 6);
+            PagedList<RegisteredUser> RegisteredUserList = InMemoryDataService.GetAll(pager);
+            return Json(RegisteredUserList, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ListPageView()
+        {
+            return new JavascriptViewResult { ViewName = "_ListPage" };
+        }
+    }
+}
---
+++ src/Samples/Advanced/JavascriptViewResultSample/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Spark.Web.Mvc;
+using Spark;
+
+namespace JavascriptViewResultSample
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+
+            ISparkSettings settings = new SparkSettings()
+            .SetAutomaticEncoding(true);
+            
+
+            SparkEngineStarter.RegisterViewEngine(settings);
+        }
+    }
+}
