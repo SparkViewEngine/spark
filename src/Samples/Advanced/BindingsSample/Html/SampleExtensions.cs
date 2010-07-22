0e84a97c4c9415598cdaa2e9675d0800932f39a1
Robert Greyling <robertgreyling@cozwecan.com>
2010-07-22 20:01:55 +0100

    Bindings final added

---
+++ src/Samples/Advanced/BindingsSample/Html/SampleExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BindingsSample.Html {
+    public static class SampleExtensions {
+        public static object Colorize(this HtmlHelper html, string text, Color min, Color max, double value) {
+            var color = Color.FromArgb(
+                Range(min.R, max.R, value),
+                Range(min.G, max.G, value),
+                Range(min.B, max.B, value));
+
+            var tagBuilder = new TagBuilder("span");
+            tagBuilder.SetInnerText(text);
+
+            tagBuilder.MergeAttribute("style", "color:" + ColorTranslator.ToHtml(color) + ";");
+            return MvcHtmlString.Create(tagBuilder.ToString());
+        }
+
+        private static int Range(int min, int max, double value) {
+            return min + (int)((max - min) * value);
+        }
+    }
+}
