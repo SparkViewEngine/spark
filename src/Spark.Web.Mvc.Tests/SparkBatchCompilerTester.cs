01e7dc5cdc70d59559055c92013f2e87bba262e3
maxild <mmaxild@gmail.com>
2010-07-22 22:23:57 +0800

    Fixed bug where Location was used to define AppBase of sandbox AppDomain.
    Some test runners (R# test runner for instance) will load assemblies from a temporary directory. This fix
    will make the test code robust when this happens.

--- src/Spark.Web.Mvc.Tests/SparkBatchCompilerTester.cs
+++ src/Spark.Web.Mvc.Tests/SparkBatchCompilerTester.cs
@@ -61,7 +61,7 @@ namespace Spark.Web.Mvc.Tests
         {
             AppDomainSetup setup = new AppDomainSetup
                                     {
-                                        ApplicationBase = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                                        ApplicationBase = Assembly.GetExecutingAssembly().GetCodeBaseDirectory()
                                     };
             AppDomain sandbox = null;
             try
@@ -197,4 +197,23 @@ namespace Spark.Web.Mvc.Tests
             return string.Format("Foo on lambda expression {0}", action);
         }
     }
+
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Get the directory where the assembly is found.
+        /// </summary>
+        /// <remarks>
+        /// This is often useful when using a runner (NCover, NUnit etc.) that 
+        /// loads assemblies from a temporary <see cref="AppDomain"/>.
+        /// </remarks>
+        public static string GetCodeBaseDirectory(this Assembly assembly)
+        {
+            string codeBaseUriString = assembly.CodeBase;
+            var uri = new UriBuilder(codeBaseUriString);
+            string codeBasePath = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(codeBasePath);
+        }
+    }
+
 }
