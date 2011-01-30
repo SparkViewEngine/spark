c28f3dc2128d716bb0e45a294cd18d35b81efdf6
RobertTheGrey <robertgreyling@gmail.com>
2011-01-30 15:44:47 +0000

    Updated version for new release

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Runtime.InteropServices;
 //------------------------------------------------------------------------------
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.1")]
-[assembly: AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: AssemblyCopyrightAttribute("Copyright (c) Louis DeJardin 2008-2010")]
+[assembly: AssemblyVersionAttribute("1.5")]
+[assembly: AssemblyFileVersionAttribute("1.5.0.0")]
+[assembly: AssemblyCopyrightAttribute("Copyright (c) Louis DeJardin 2008-2011")]
 [assembly: AssemblyProductAttribute("Spark")]
 [assembly: AssemblyCompanyAttribute("Louis DeJardin")]
 [assembly: AssemblyConfigurationAttribute("release")]
-[assembly: AssemblyInformationalVersionAttribute("1.1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.5.0.0")]
 
