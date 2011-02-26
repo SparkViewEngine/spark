caec3eda76ebd4cfd0329222b5538fd1bc9f29a0
RobertTheGrey <robertgreyling@gmail.com>
2011-02-26 22:28:24 +0000

    Preparing for release 1.5.1

---
+++ NuGet/Spark.Web.Mvc2/1.5.1/Content/AppStart_SparkWebMvc.cs.pp
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Spark;
+using Spark.Web.Mvc;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.AppStart_SparkWebMvc), "Start")]
+
+namespace $rootnamespace$ {
+    public static class AppStart_SparkWebMvc {
+        public static void Start() {
+            var settings = new SparkSettings();
+            settings.SetAutomaticEncoding(true); 
+
+            // Note: you can change the list of namespace and assembly
+            // references in Views\Shared\_global.spark
+            SparkEngineStarter.RegisterViewEngine(settings);
+        }
+    }
+}
