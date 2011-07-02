8742c24f622a82a08165af9531af47690af61138
RobertTheGrey <robertgreyling@gmail.com>
2011-07-02 17:13:19 +0100

    Version update v.16

--- NuGet/PackAndPush.bat
+++ NuGet/PackAndPush.bat
@@ -2,8 +2,8 @@ nuget pack Spark\Spark.nuspec
 nuget pack Spark.Web.Mvc2\Spark.Web.Mvc2.nuspec
 nuget pack Spark.Web.Mvc3\Spark.Web.Mvc3.nuspec
 
-nuget push Spark.1.5.1.6.nupkg
-nuget push Spark.Web.Mvc2.1.5.1.6.nupkg
-nuget push Spark.Web.Mvc3.1.5.1.6.nupkg
+nuget push Spark.1.6.nupkg
+nuget push Spark.Web.Mvc2.1.6.nupkg
+nuget push Spark.Web.Mvc3.1.6.nupkg
 
 pause
