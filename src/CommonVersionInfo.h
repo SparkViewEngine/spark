fd03a1f44b5620b3141907ba247e7ceb8de82879
Robert Greyling <robertgreyling@gmail.com>
2012-09-15 13:11:28 +0100

    Update to v1.7.4.0

--- src/CommonVersionInfo.h
+++ src/CommonVersionInfo.h
@@ -1,9 +1,1 @@
-// this is an auto-generated file
-#define VERSIONINFO_VERSIONSTRING "1.7.3.0"
-#define VERSIONINFO_MAJOR 1
-#define VERSIONINFO_MINOR 7
-#define VERSIONINFO_BUILD 3
-#define VERSIONINFO_REVISION 0
-#define VERSIONINFO_COPYRIGHT "Copyright (c) Louis DeJardin 2008-2012"
-#define VERSIONINFO_COMPANY "Louis DeJardin"
-#define VERSIONINFO_PRODUCT "Spark"
+// this is an auto-generated file#define VERSIONINFO_VERSIONSTRING "1.7.4.0"#define VERSIONINFO_MAJOR 1#define VERSIONINFO_MINOR 7#define VERSIONINFO_BUILD 4#define VERSIONINFO_REVISION 0#define VERSIONINFO_COPYRIGHT "Copyright (c) Louis DeJardin 2008-2012"#define VERSIONINFO_COMPANY "Louis DeJardin"#define VERSIONINFO_PRODUCT "Spark"
