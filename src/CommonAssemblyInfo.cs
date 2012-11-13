5e20116b561282dd9ba7d00b95d4b5f7de0fda9b
Robert Greyling <robertgreyling@gmail.com>
2012-11-13 11:23:10 +0000

    Update to v1.7.5.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -14,10 +14,10 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
 [assembly: AssemblyVersionAttribute("1.7")]
-[assembly: AssemblyFileVersionAttribute("1.7.4.0")]
+[assembly: AssemblyFileVersionAttribute("1.7.5.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) Louis DeJardin 2008-2012")]
 [assembly: AssemblyProductAttribute("Spark")]
 [assembly: AssemblyCompanyAttribute("Louis DeJardin")]
 [assembly: AssemblyConfigurationAttribute("release")]
-[assembly: AssemblyInformationalVersionAttribute("1.7.4.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.7.5.0")]
 
