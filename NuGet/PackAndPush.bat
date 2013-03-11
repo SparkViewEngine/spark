f7ca99c5eb5605349e0c13442cbcc846c5798974
Robert Greyling <robertgreyling@gmail.com>
2013-03-11 22:07:36 +0000

    Added MVC4 nuget

--- NuGet/PackAndPush.bat
+++ NuGet/PackAndPush.bat
@@ -1,9 +1,11 @@
 nuget pack Spark\Spark.nuspec
 nuget pack Spark.Web.Mvc2\Spark.Web.Mvc2.nuspec
 nuget pack Spark.Web.Mvc3\Spark.Web.Mvc3.nuspec
+nuget pack Spark.Web.Mvc4\Spark.Web.Mvc4.nuspec
 
 nuget push Spark.1.7.5.1.nupkg
 nuget push Spark.Web.Mvc2.1.7.5.1.nupkg
 nuget push Spark.Web.Mvc3.1.7.5.1.nupkg
+nuget push Spark.Web.Mvc4.1.0.0.0.nupkg
 
 pause
