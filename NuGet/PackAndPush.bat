34321d0a1cc1db705b4a378ca5c007c4a9d0af2b
Robert Greyling <robertgreyling@gmail.com>
2013-11-21 10:10:06 +0000

    Update to v1.7.5.3

--- NuGet/PackAndPush.bat
+++
@@ -1,9 +0,0 @@
-nuget pack Spark\Spark.nuspec
-nuget pack Spark.Web.Mvc2\Spark.Web.Mvc2.nuspec
-nuget pack Spark.Web.Mvc3\Spark.Web.Mvc3.nuspec
-
-nuget push Spark.1.7.5.1.nupkg
-nuget push Spark.Web.Mvc2.1.7.5.1.nupkg
-nuget push Spark.Web.Mvc3.1.7.5.1.nupkg
-
-pause
