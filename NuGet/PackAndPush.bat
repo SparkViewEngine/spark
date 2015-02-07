e778116f846d5c03950b63c26b4ce2f9108509ff
Robert Greyling <robertgreyling@gmail.com>
2015-02-07 23:24:07 +0000

    Update to v1.8.0.0

--- NuGet/PackAndPush.bat
+++ NuGet/PackAndPush.bat
@@ -2,10 +2,12 @@ nuget pack Spark\Spark.nuspec
 nuget pack Spark.Web.Mvc2\Spark.Web.Mvc2.nuspec
 nuget pack Spark.Web.Mvc3\Spark.Web.Mvc3.nuspec
 nuget pack Spark.Web.Mvc4\Spark.Web.Mvc4.nuspec
+nuget pack Spark.Web.Mvc5\Spark.Web.Mvc5.nuspec
 
-nuget push Spark.1.7.5.3.nupkg
-nuget push Spark.Web.Mvc2.1.7.5.3.nupkg
-nuget push Spark.Web.Mvc3.1.7.5.3.nupkg
-nuget push Spark.Web.Mvc4.1.7.5.3.nupkg
+nuget push Spark.1.8.0.0.nupkg
+nuget push Spark.Web.Mvc2.1.8.0.0.nupkg
+nuget push Spark.Web.Mvc3.1.8.0.0.nupkg
+nuget push Spark.Web.Mvc4.1.8.0.0.nupkg
+nuget push Spark.Web.Mvc5.1.8.0.0.nupkg
 
 pause
