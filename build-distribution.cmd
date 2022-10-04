c882369d326eb77df990d1a4ae2667b5cbfe6f11
bounav <benjamin.fourmond@comsechq.com>
2022-10-04 15:04:25 +0100

    Using msbuild to build and generate nuget packages
    - Extra cleanup test that clear the bin and obj folders
    - No longer building everthing into /build/framework/platform/project/release folder
    - Msbuild cleans then builds the solution
    - After running the unit tests msbuild generates the nuget packages
    - The only output of successful build are the nuget packages in /build

--- build-distribution.cmd
+++ build-distribution.cmd
@@ -1,6 +1,6 @@
 if "%1"=="" build-distribution 1
 :start
 REM Assumes nant is installed and in your path 
-nant.exe -f:spark.build build package -D:build.number=%1
+nant.exe -f:spark.build build package push -D:build.number=%1
 pause
 goto start
